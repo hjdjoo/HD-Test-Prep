ebc646cd113d5384e20041a6a1f1fb668ea3a01d
Darryl <hjdjoo@gmail.com>
2024-10-09 15:22:40 -0400

    Testing simple API endpoint

--- server/server.ts
+++ server/server.ts
@@ -8,7 +8,10 @@ const app: Application = express();
 app.use(express.json());
 app.use(express.urlencoded());
 
-// app.use("/")
+app.use("/api", (_req: Request, res: Response) => {
+
+  res.status(200).json("Received")
+})
 
 app.use("/*", (_req: Request, res: Response) => {
 
@@ -17,5 +20,5 @@ app.use("/*", (_req: Request, res: Response) => {
 })
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`)
+  console.log(`Server listening on port ${PORT}`);
 });
