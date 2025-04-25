a04a562de4189aaed4c7fade61c026ff4dd7118b
Darryl <hjdjoo@gmail.com>
2025-04-25 17:04:33 -0400

    Added a console log to check something...

--- src/config.ts
+++ src/config.ts
@@ -2,3 +2,5 @@ export const SERVER_URL =
   import.meta.env.MODE === "development" ?
     import.meta.env.VITE_SERVER_URL_DEV.concat("/api") :
     import.meta.env.VITE_SERVER_URL_PROD;
+
+console.log("SERVER_URL: ", SERVER_URL)
